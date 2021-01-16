23cc107ce4a07da7232fb20ab9d6b1879d0b28db
Ian C <ian@thecommittedbug.io>
2021-01-15 20:12:40 -0500

    amend .gitignore to include IDE config files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import 'assets/scss/admin.scss'
+import Header from './components/layout/Header'
+import Home from './components/home'
+import SideBar from 'components/layout/SideBar'
+import Menu from 'components/menu'
+import { ModalProvider } from 'context/ModalProvider'
+
+const App = () => {
+	return (
+		<ModalProvider>
+			<Router>
+				<div className='app-wrapper'>
+					<SideBar />
+					<div className='page'>
+						<Header />
+						<div className='container'>
+							<Switch>
+								<Route exact path='/' component={Home} />
+								<Route exact path='/menu' component={Menu} />
+							</Switch>
+						</div>
+					</div>
+				</div>
+			</Router>
+		</ModalProvider>
+	)
 }
-
-export default App;
+export default App
