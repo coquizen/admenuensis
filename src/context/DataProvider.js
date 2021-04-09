bc51a68fe9f4d322a92443bcaabaa9f54984368f
Ian C <ian@thecommittedbug.io>
2021-04-09 10:01:29 -0400

    update data api

--- src/context/DataProvider.js
+++ src/context/DataProvider.js
@@ -1,40 +1,37 @@
 /** @format */
 
 import React, { createContext, useContext, useEffect, useState } from 'react'
-
-import * as api from 'utils/api'
+import useReactContextDevTool from 'hooks/useReactContextDevTool'
+import * as api from 'services/data'
 
 const DataContext = createContext()
 
 const DataProvider = ({ children }) => {
-	const [allSectionsData, setAllSectionsData] = useState(null)
-	const [mappedSections, setMappedSections] = useState(null)
-	const [allItemsData, setAllItemsData] = useState(null)
-	const [isDirty, setIsDirty] = useState(true)
+	const [ allSectionsData, setAllSectionsData ] = useState(null)
+	const [ menus, setMenusData ] = useState(null)
+	const [ allItemsData, setAllItemsData ] = useState(null)
+	const [ isDirty, setIsDirty ] = useState(true)
 	useEffect(() => {
 		if (isDirty) {
 			api.fetchSections().then((data) => setAllSectionsData(data))
-			api.fetchMappedSections().then((data) => setMappedSections(data))
-
+			api.fetchMenus().then((data) => setMenusData(data))
 			api.fetchItems().then((data) => setAllItemsData(data))
 			setIsDirty(false)
 		}
-	}, [isDirty])
+	}, [ isDirty ])
+
 	const getSectionDataByID = (id) => allSectionsData && allSectionsData.find((section) => section.id === id)
 
 	const getItemDataByID = (id) => allItemsData && allItemsData.find((item) => item.id === id)
 
 	const rootSections =
 		allSectionsData &&
-		allSectionsData.filter((section) => section.section_parent_id === '00000000-0000-0000-0000-000000000000')
+		allSectionsData.filter((section) => section.section_id === undefined)
 
 	var allData = {}
-	allData['section'] = allSectionsData
-	allData['item'] = allItemsData
+	allData[ 'section' ] = allSectionsData
+	allData[ 'item' ] = allItemsData
 
-	const reSortTable = (activeID, overID, isBefore) => {
-		api.reSortTable(activeID, overID, isBefore).then(setIsDirty(true))
-	}
 
 	const updateSection = (data) => {
 		api.updateSection(data).then(setIsDirty(true))
@@ -43,18 +40,33 @@ const DataProvider = ({ children }) => {
 		api.updateItem(data).then(setIsDirty(true))
 	}
 
+	useReactContextDevTool({
+		id: 'dataprovider',
+		displayName: 'Data',
+		values: {
+			allSectionsData,
+			rootSections,
+			updateSection,
+			getItemDataByID,
+			getSectionDataByID,
+			updateItem,
+			allData,
+			menus
+		},
+	})
+
+
 	return (
 		<DataContext.Provider
 			value={{
 				allSectionsData,
 				rootSections,
-				mappedSections,
-				reSortTable,
 				updateSection,
 				getItemDataByID,
 				getSectionDataByID,
 				updateItem,
 				allData,
+				menus
 			}}>
 			{children}
 		</DataContext.Provider>
---
+++ src/services/data.js
@@ -0,0 +1,84 @@
+/** @format */
+
+const headers = {
+	'Content-Type': 'application/json',
+}
+export const authHeader = () => {
+	const token = JSON.parse(localStorage.getItem('authToken'))
+	if (token) {
+		return {
+			"Authorization": "Bearer " + token,
+			'Content-Type': 'application/json',
+		}
+	} else {
+		return {};
+	}
+}
+
+export const fetchSections = () => {
+	return fetch('/api/v1/sections')
+		.then((response) => response.json())
+		.then((data) => data.data)
+		.catch((error) => console.error(error))
+}
+
+export const fetchSection = (id) => {
+	return fetch(`/api/v1/sections/${id}`)
+		.then((response) => response.json())
+		.then((data) => data.data)
+}
+export const fetchItems = () => {
+	return fetch(`/api/v1/items`)
+		.then((response) => response.json())
+		.then((data) => data.data)
+}
+export const fetchItem = (id) => {
+	return fetch(`/api/v1/items/${id}`)
+		.then((response) => response.json())
+		.then((data) => data.data)
+		.catch((error) => console.error(error))
+}
+
+export const updateSection = (data) => {
+	return fetch(`/api/v1/sections/${data.id}`, {
+		method: 'PATCH',
+		headers: authHeader(),
+		body: JSON.stringify(data),
+	}).then((response) => response.json())
+		.then((data) => data.data)
+		.catch((error) => {
+			console.error(error)
+		})
+}
+
+export const updateItem = (data) => {
+	return fetch(`/api/v1/items/${data.id}`, {
+		method: 'PATCH',
+		headers: authHeader(),
+		body: JSON.stringify(data),
+	})
+		.then((response) => response.json())
+		.then((data) => data.data)
+		.catch((error) => {
+			console.error(error)
+		})
+}
+
+export const deleteSection = (id) => {
+	return fetch('/api/v1/sections/${id}', {
+		method: 'DELETE',
+		headers: authHeader(),
+	})
+}
+export const deleteItem = (id) => {
+	return fetch('/api/v1/items/${id}', {
+		method: 'DELETE',
+		headers: authHeader(),
+	})
+}
+
+export const fetchMenus = () => {
+	return fetch(`/api/v1/menus`)
+		.then((response) => response.json())
+		.then((data) => data.data)
+}
