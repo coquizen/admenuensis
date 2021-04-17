a1840b8980c0c3cfd020dae7ae8ef3258c0ec3df
Ian C <ian@thecommittedbug.io>
2021-04-17 09:15:50 -0400

    Add Settings placeholder

---
+++ src/pages/Settings/Settings.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from './Settings.module.scss'
+
+const Settings = () => {
+    return (
+        <div className={styles.Settings}>
+            <div className={styles.SettingsHeader}>
+                Overview
+            </div>
+            <div className={styles.SettingsBody}>
+
+            </div>
+        </div>
+    )
+}
+
+export default Settings
